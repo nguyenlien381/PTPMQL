4ae29c944f1a0c0c5f0e042dd57674a08f769a40
nguyenlien381 <nguyenlienn22032002@gmail.com>
2024-08-20 11:12:11 +0700

    CreatProject

--- DemoMVC/Controllers/HomeController.cs
+++ DemoMVC/Controllers/HomeController.cs
@@ -17,6 +17,13 @@ public class HomeController : Controller
     {
         return View();
     }
+    [HttpPost]
+    public IActionResult Index(string FullName )
+    {
+        string strOutput = "Xin chào" + "-" + FullName;
+        ViewBag.hghh = strOutput;
+        return View();
+    }
 
     public IActionResult Privacy()
     {
