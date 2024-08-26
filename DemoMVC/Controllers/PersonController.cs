a6968ea0ffdfe6ea9f22d8323f86711b56ea69ae
nguyenlien381 <nguyenlienn22032002@gmail.com>
2024-08-26 17:09:07 +0700

    CraetProjct

--- DemoMVC/Controllers/PersonController.cs
+++ DemoMVC/Controllers/PersonController.cs
@@ -1,4 +1,22 @@
+
+using Microsoft.AspNetCore.Mvc;
+using DemoMVC.Models;
+
 namespace DemoMVC.Controllers
 {
-    
+    public class PersonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Person ps)
+        {
+            string strOutput = "Xinchao:" +  ps.PersonId + "_" + ps.FullName +"_" + ps.Address;
+            ViewBag.fgjk = strOutput;
+            return View();
+        }
+
+    }
 }
--- DemoMVC/obj/Debug/net8.0/DemoMVC.AssemblyInfo.cs
+++ DemoMVC/obj/Debug/net8.0/DemoMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc39a298a1f73b0e9766bc2cbaecabec8eca5659")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d11457ed66905549747fcd514c8c11c8b3994970")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
